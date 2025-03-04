8ec6bb12d927f15b736e4d340ab601156e9ecf72
Jack Rellinger <rellingerjack@gmail.com>
2025-03-04 16:21:08 -0500

    changed home page to use link tag for routing

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,13 +1,14 @@
 // src/Components/Home/HomePage.jsx
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <div>
       <h1>Welcome to the Yoga Sign-Up Platform</h1>
       <ul>
-        <li><a href="/users">Manage Users</a></li>
-        <li><a href="/classes">Manage Classes</a></li>
+        <li><Link to="/users">Manage Users</Link></li>
+        <li><Link to="/classes">Manage Classes</Link></li>
       </ul>
     </div>
   );
