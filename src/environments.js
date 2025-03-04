d06e19d76ed42c67e728ed250e96afefee28d050
Jack Rellinger <rellingerjack@gmail.com>
2025-03-03 20:29:13 -0500

    Remove environments.js from repository tracking

--- src/environments.js
+++
@@ -1,7 +0,0 @@
-const Env = {
-    APPLICATION_ID: "LdC2e27NQ7jipj05LNfiDr9RbNoManjsfzwsPIbw", // add from back4app
-    JAVASCRIPT_KEY: "ZGZT9lflaPJDtj4NlZkPyu3Vdpu635r69Hrif0Jc",
-    SERVER_URL: "https://parseapi.back4app.com"
-};
-
-export default Env;
