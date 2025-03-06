95b8d525fd2fd310cbdeb4fa3ff5a89a27830629
Danny Mikolay <105239322+dmikolay@users.noreply.github.com>
2025-03-06 18:08:25 -0500

    Commented on ClassUserList.jsx

--- src/Components/Class/ClassUserList.jsx
+++ src/Components/Class/ClassUserList.jsx
@@ -1,5 +1,4 @@
- 
-import React from "react";
+ import React from "react";
 
 // Component to display a list of users enrolled in a class
 const ClassUserList = ({ users }) => {
@@ -16,28 +15,3 @@ const ClassUserList = ({ users }) => {
 };
 
 export default ClassUserList;
-
-
-import React from "react";
-
-// Component to display a list of classes with actions for editing, deleting, and viewing users
-const ClassList = ({ classes, onEdit, onDelete, onShowUsers }) => {
-  return (
-    <ul>
-      {classes.map((cls) => (
-        <li key={cls.id}>
-          {/* Display class name */}
-          <strong>{cls.get("name")}</strong>
-          {/* Button to edit the class */}
-          <button onClick={() => onEdit(cls)}>Edit</button>
-          {/* Button to delete the class */}
-          <button onClick={() => onDelete(cls.id)}>Delete</button>
-          {/* Button to view users signed up for this class */}
-          <button onClick={() => onShowUsers(cls)}>Show Users</button>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-export default ClassList;
