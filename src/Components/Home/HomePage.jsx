6d8cbc70d03ce22785b8ce3c6d4f3af087f0eeed
Danny Mikolay <105239322+dmikolay@users.noreply.github.com>
2025-03-06 18:09:44 -0500

    Commented on HomePage.jsx

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -2,11 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Home page component providing navigation to different sections of the app
 export default function HomePage() {
   return (
     <div>
+      {/* Main heading of the homepage */}
       <h1>Welcome to the Yoga Sign-Up Platform</h1>
       <ul>
+        {/* Navigation links to user and class management pages */}
         <li><Link to="/users">Manage Users</Link></li>
         <li><Link to="/classes">Manage Classes</Link></li>
       </ul>
